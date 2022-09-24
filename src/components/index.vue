9d5a085a4a8aac053be26cb744f6ad0508221267
dov.ji <dov.ji@zoom.us>
2022-09-24 22:09:10 +0800

    init

---
+++ src/components/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <inner-index />
+  </div>
+</template>
+<script>
+  /* eslint-disable */
+  import InnerIndex from './inner/index.vue'
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    },
+    data () {
+      return {
+        Halo: 'heiheihei'
+      }
+    },
+    created () {
+      debugger
+    },
+    components: {
+      InnerIndex
+    },
+    methods: {
+      sayHello () {
+        console.log(this.Halo)
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/inner/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="inner">
+    this is inner index
+  </div>
+</template>
+<script>
+/* eslint-disable */
+export default {
+  mounted () {
+    debugger;
+  }
+}
+</script>
+<style lang='scss' scoped>
+</style>
